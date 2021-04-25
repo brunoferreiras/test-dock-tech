2ab77cc2c45ca0b74a071bdb3f6e7a66b0bbefd6
Bruno Ferreira <bruno.ferreira@fortics.com.br>
2021-04-25 09:28:20 -0300

    feat: add alias path and optmize jest

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+module.exports = {
+  roots: ['<rootDir>/src', '<rootDir>/test'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,tsx,ts}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!src/**/*.type.ts',
+    '!src/**/*.interface.ts',
+    '!src/**/*.config.ts',
+    '!src/**/index.ts',
+    '!src/*'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'html'],
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '@tests/(.*)': '<rootDir>/tests/$1'
+  }
+}
