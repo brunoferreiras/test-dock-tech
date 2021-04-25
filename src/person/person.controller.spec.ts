0c810b54488228fe6d5f1e8ab05c7273e8b22d53
Bruno Ferreira <bruno.ferreira@fortics.com.br>
2021-04-25 11:32:24 -0300

    test: add person controller

--- src/person/person.controller.spec.ts
+++ src/person/person.controller.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { PersonController } from './person.controller'
 import { PersonService } from './person.service'
+import { CreatePersonDto } from './dto/create-person.dto'
 
 describe('PersonController', () => {
   let controller: PersonController
+  let service: PersonService
 
   beforeEach(async () => {
     const serviceMock = {
@@ -21,9 +23,30 @@ describe('PersonController', () => {
     }).compile()
 
     controller = module.get<PersonController>(PersonController)
+    service = module.get<PersonService>(PersonService)
   })
 
   it('should be defined', () => {
     expect(controller).toBeDefined()
+  })
+
+  describe('create', () => {
+    it('should be called repository with correct params', async () => {
+      const dto = {
+        cpf: '12345678911',
+        birthday_date: '2020-02-20',
+        password: 'any'
+      } as CreatePersonDto
+      await controller.create(dto)
+      expect(service.create).toBeCalledWith(dto)
+      expect(service.create).toBeCalledTimes(1)
+    })
+  })
+
+  describe('findAll', () => {
+    it('should be called repository with correct params', async () => {
+      await controller.findAll()
+      expect(service.findAll).toBeCalledTimes(1)
+    })
   })
 })
